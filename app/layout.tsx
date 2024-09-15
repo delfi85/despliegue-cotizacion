f2aa65e08d9245812109186b2a1be4aa7510152a
delfi85 <134985888+delfi85@users.noreply.github.com>
2024-09-15 00:27:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Toaster } from 'react-hot-toast';
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Aceptar Cotización",
+  description: "TP6 - G7 - ISW - UTN FRC - 2024",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Toaster />
+        {children}
+      </body>
+    </html>
+  );
+}
